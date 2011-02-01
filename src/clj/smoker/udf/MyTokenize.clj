58708ac8f384ccbe3ea5bc719434eb57795e2c5c
Nate Murray <nate@natemurray.com>
2011-02-01 13:30:11 -0800

    refactored the tokenizer to make the clojure code more obvious

--- src/clj/smoker/udf/MyTokenize.clj
+++ src/clj/smoker/udf/MyTokenize.clj
@@ -1,6 +1,7 @@
 
 (ns smoker.udf.MyTokenize
   (:import [smoker.udf ClojureUDTF])
+  (:require [smoker.udtf.gen :as gen])
   (:import [java.util ArrayList List])
   (:import [org.apache.hadoop.hive.ql.exec UDFArgumentException])
   (:import [org.apache.hadoop.hive.ql.metadata HiveException])
@@ -15,40 +16,44 @@
   (:import [java.util Date])
   (:require [clojure.contrib.str-utils2 :as su]))
 
-(gen-class
- :name smoker.udf.MyTokenize
- :extends smoker.udf.ClojureUDTF
- :init init
- :constructors {[] []}
- :state state
- )
+;; (gen-class
+;;  :name smoker.udf.MyTokenize
+;;  :extends smoker.udf.ClojureUDTF
+;;  :init init
+;;  :constructors {[] []}
+;;  :state state
+;;  )
 
-(defn -init []
-  [[] (ref {:stringIO nil})])
+;; (defn -init []
+;;   [[] (ref {:stringIO nil})])
 
-(defn -initialize [this args]
-  (if (not (= (count args) 1))
-   (throw 
-      (UDFArgumentException. "tokenize() takes exactly one argument")))
-  (dosync (alter (.state this) assoc :stringIO (nth args 0)))
-  (let [fieldNames (ArrayList. ["word" "count"])
-        fieldIOs (ArrayList. 
-          [PrimitiveObjectInspectorFactory/javaStringObjectInspector
-          PrimitiveObjectInspectorFactory/javaIntObjectInspector])]
-    (ObjectInspectorFactory/getStandardStructObjectInspector 
-       fieldNames fieldIOs)))
+;; (defn -initialize [this args]
+;;   (if (not (= (count args) 1))
+;;    (throw 
+;;       (UDFArgumentException. "tokenize() takes exactly one argument")))
+;;   (dosync (alter (.state this) assoc :stringIO (nth args 0)))
+;;   (let [fieldNames (ArrayList. ["word" "count"])
+;;         fieldIOs (ArrayList. 
+;;           [PrimitiveObjectInspectorFactory/javaStringObjectInspector
+;;           PrimitiveObjectInspectorFactory/javaIntObjectInspector])]
+;;     (ObjectInspectorFactory/getStandardStructObjectInspector 
+;;        fieldNames fieldIOs)))
 
-(defn tokenize [line]
+
+(defn process [line]
   (map 
    (fn [token] [token (Integer/valueOf 1)]) 
    (su/split line #"\s+")))
 
-(defn -process [this record]
-  (if-let [document (.getPrimitiveJavaObject 
-                     (@(.state this) :stringIO) (nth record 0))]
-    (doall (map (fn [results] 
-                  (.emit this (into-array Object results))) 
-                (tokenize document)))))
+(gen/gen-udtf)
+(gen/gen-wrapper-methods)
 
-(defn -close [this])
+;; (defn -process [this record]
+;;   (if-let [document (.getPrimitiveJavaObject 
+;;                      (@(.state this) :stringIO) (nth record 0))]
+;;     (doall (map (fn [results] 
+;;                   (.emit this (into-array Object results))) 
+;;                 (tokenize document)))))
+
+;; (defn -close [this])
 
