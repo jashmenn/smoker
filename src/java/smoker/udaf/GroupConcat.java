bace0a1fe2e720755884c1fe4eb682ca8cb65099
Nate Murray <nate@natemurray.com>
2011-04-20 12:35:39 -0700

    formatting

--- src/java/smoker/udaf/GroupConcat.java
+++ src/java/smoker/udaf/GroupConcat.java
@@ -13,55 +13,56 @@ import java.util.Arrays;
 public class GroupConcat extends UDAF{
 
   public static class GroupConcatStringEvaluator implements UDAFEvaluator {
-                private Text mOutput;
-                private boolean mEmpty;
+    private Text mOutput;
+    private boolean mEmpty;
 
-        public GroupConcatStringEvaluator() {
-                super();
-                init();
-        }
+    public GroupConcatStringEvaluator() {
+      super();
+      init();
+    }
 
-        public void init() {
-                mOutput = null;
-                mEmpty = true;
+    public void init() {
+      mOutput = null;
+      mEmpty = true;
+    }
+
+    public boolean iterate(Text o,  IntWritable N) {
+      if (o!=null) {
+        if(mEmpty) {
+          mOutput = new Text(N+" "+o.toString());
+          mEmpty = false;
+        } else { String temp = mOutput.toString() + "\t" + N + " " + o.toString();
+          String[] split = temp.split("\t");
+          Arrays.sort(split);
+          String sorted = split[0];
+          for (int i = 1; i < split.length; i++)
+            {
+              sorted = sorted + "\t" + split[i];
+            }
+          mOutput.set(sorted);
         }
-        public boolean iterate(Text o,  IntWritable N) {
-                if (o!=null) {
-                        if(mEmpty) {
-                                mOutput = new Text(N+" "+o.toString());
-                                mEmpty = false;
-                        } else { String temp = mOutput.toString() + "\t" + N + " " + o.toString();
-                                String[] split = temp.split("\t");
-                                Arrays.sort(split);
-                                String sorted = split[0];
-                                for (int i = 1; i < split.length; i++)
-                                {
-                                        sorted = sorted + "\t" + split[i];
-                                }
-                                mOutput.set(sorted);
-                        }
-                }
-                return true;
+      }
+      return true;
+    }
+    public Text terminatePartial() {return mEmpty ? null : mOutput;}
+    public boolean merge(Text o) {
+      if (o!=null) {
+        if(mEmpty) {
+          mOutput = new Text(o.toString());
+          mEmpty = false;
+        } else { String temp = mOutput.toString() + "\t" + o.toString();
+          String[] split = temp.split("\t");
+          Arrays.sort(split);
+          String sorted = split[0];
+          for (int i = 1; i < split.length; i++)
+            {
+              sorted = sorted + "\t" + split[i];
+            }
+          mOutput.set(sorted);
         }
-        public Text terminatePartial() {return mEmpty ? null : mOutput;}
-        public boolean merge(Text o) {
-                if (o!=null) {
-                        if(mEmpty) {
-                            mOutput = new Text(o.toString());
-                            mEmpty = false;
-                        } else { String temp = mOutput.toString() + "\t" + o.toString();
-                                String[] split = temp.split("\t");
-                                Arrays.sort(split);
-                                String sorted = split[0];
-                                for (int i = 1; i < split.length; i++)
-                                {
-                                        sorted = sorted + "\t" + split[i];
-                                }
-                                mOutput.set(sorted);
-                        }
-                }
-                return true;
-        }
-        public Text terminate() {return mEmpty ? null : mOutput;}
-}
+      }
+      return true;
+    }
+    public Text terminate() {return mEmpty ? null : mOutput;}
+  }
 }
