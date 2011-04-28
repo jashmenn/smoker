1938d055d44187de6c2691468535a80d0ec66ba4
Nate Murray <nate@natemurray.com>
2011-04-28 10:24:38 -0700

    tweaks to make it work publically

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject com.atti.wdm/smoker "1.0.1-SNAPSHOT"
+(defproject com.combinator/smoker "1.0.2"
   :description "When you visit your Hive, bring your smoker and you'll get stung less"
   :dependencies 
   [[org.clojure/clojure "1.2.0"]
    [org.clojure/clojure-contrib "1.2.0"]
    [url-normalizer "0.0.4"]
    [net.htmlparser.jericho/jericho-html "3.1"]
-   [clj-html-parser "0.1.6"]
    [commons-logging "1.0.4"]]
   :dev-dependencies
   [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
@@ -13,11 +12,9 @@
      :exclusions [org.clojure/clojure-contrib]]
    [lein-javac "1.2.1-SNAPSHOT"
       :exclusions [org.clojure/clojure-contrib]]
-   [swank-clojure "1.3.0-SNAPSHOT"]
-
+   [swank-clojure "1.3.0"]
    [robert/hooke "1.0.2"]
    [lein-daemon "0.2.1"]
-
    ;; start hive deps - include these deps in your project if you want
    ;; to use the hive functionality. they wont be in the uberjar
    [hive/hive-common "0.5.0"]
@@ -32,7 +29,7 @@
    [commons-lang/commons-lang "2.5"]
    [jline "0.9.94"]
    [org.antlr/antlr "3.0.1"]
-   [fb/libfb303 "3.0.3"]
+   [fb/libfb303 "1.1"]
    [org.datanucleus/datanucleus-core "1.1.2"]
    [org.datanucleus/datanucleus-rdbms "1.1.2"]
    [org.datanucleus/datanucleus-connectionpool "1.0.2"]
@@ -45,12 +42,7 @@
    ;; end hive deps
    ]
   :repositories
-    {"clojars" "http://clojars.org/repo"
-     "yp"      "http://maven.corp.atti.com:9999/nexus/content/groups/public"
-     "YP-3p" "http://svn2.wc1.yellowpages.com:9999/nexus/content/repositories/thirdparty"
-     "YP-releases" "http://svn2.wc1.yellowpages.com:9999/nexus/content/repositories/releases"
-     "YP-snap" "http://svn2.wc1.yellowpages.com:9999/nexus/content/repositories/snapshots"
-     }
+    {"clojars" "http://clojars.org/repo"}
   :compile-path "build/classes"
   :target-dir "build"
   :java-source-path "src/java"
